93836f930963039deef936bde15c6cbef6658f7c
Sejin Kim <kimsejin2118@gmail.com>
2023-05-13 17:38:49 +0900

    0513

--- graph_and_traversal/7576.cpp
+++ graph_and_traversal/7576.cpp
@@ -1,55 +1,37 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <queue>
 #define fast_io ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 #define endl '\n'
 
-int m, n, ans, num_of_ripe_tomato;
+int m, n, ans;
 int move_row[4] = {1, -1, 0, 0};
 int move_col[4] = {0, 0, 1, -1};
 vector<vector<int> > box;
 vector<vector<int> > vistied;
 queue<pair<int, int> > q;
 
-void bfs(int row, int col)
+void bfs()
 {
-	q.push(make_pair(row, col));
-	vistied[row][col] += 1;
+	// vistied[q.front().first][q.front().second] = 1;
 	while (!q.empty())
 	{
 		int r = q.front().first;
 		int c = q.front().second;
 		q.pop();
-		if (num_of_ripe_tomato == 0)
-		{
-			ans = vistied[r][c];
-			return;
-		}
 		for(int i = 0; i<4; i++)
 		{
 			int x = r + move_row[i];
 			int y = c + move_col[i];
-			if (x < 0 || x > n - 1 || y < 0 || y > m - 1 || vistied[x][y] != 0)
+			if (x < 0 || x > n - 1 || y < 0 || y > m - 1 || box[x][y] != 0)
 				continue;
-			if (box[x][y] == 0)
+			if (vistied[x][y] == 0)
 			{
 				q.push(make_pair(x, y));
 				vistied[x][y] = vistied[r][c] + 1;
-				num_of_ripe_tomato--;
 			}
-		}
-	}
-}
-
-void run_bfs()
-{
-	for(int i = 0; i<n; i++)
-	{
-		for(int j = 0; j<m; j++)
-		{
-			if (box[i][j] == 1)
-				bfs(i, j);
 		}
 	}
 }
@@ -61,16 +43,33 @@ int main()
 	vistied.resize(n + 1);
 	for(int i = 0; i<n; i++)
 	{
+		vistied[i].resize(m + 1);
 		for(int j = 0; j<m; j++)
 		{
 			int tomato;
 			cin >> tomato;
-			if (tomato == 0)
-				num_of_ripe_tomato++;
 			box[i].push_back(tomato);
+			if (tomato == 1)
+				q.push(make_pair(i, j));	//같은 날에 서로 다른 곳에서 토마토가 익을 수수
+			//있으므로 큐에 넣어서 순서를 준다.
+			// vistied[i][j] = 0;
 		}
 	}
-
-	run_bfs();
+	bfs();
+	for(int i = 0; i<n; i++)
+	{
+		for(int j = 0; j<m; j++)
+		{
+			if (vistied[i][j] == 0 && box[i][j] == 0)
+			{
+				ans = -1;
+				break;
+			}	
+			else 
+				ans = max(ans, vistied[i][j]);
+		}
+		if (ans == -1)
+			break;
+	}
 	cout << ans;
 }
