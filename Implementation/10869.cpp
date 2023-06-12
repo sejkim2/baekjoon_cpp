bc4b51efa9b3cf586cdc8a3bdf2ada9d88bc684b
Sejin Kim <kimsejin2118@gmail.com>
2023-06-12 20:25:26 +0900

    0612

---
+++ Implementation/10869.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define fast_io ios_base::sync_with_stdio(false), cin.tie(false), cout.tie(false)
+#define endl '\n'
+using namespace std;
+
+int A, B;
+
+int main()
+{
+	cin >> A >> B;
+	cout << A+B << endl;
+	cout << A-B << endl;
+	cout << A*B << endl;
+	cout << A/B << endl;
+	cout << A%B << endl;
+}
---
+++ two_pointer/1644.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#define fast_io ios_base::sync_with_stdio(false), cin.tie(false), cout.tie(false)
+#define endl '\n'
+using namespace std;
+
