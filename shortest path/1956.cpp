d365a843d090ddc6ae31cacb4ac67d5fded46dd7
kimsejin <kimsejin2118@gmail.com>
2023-06-23 17:10:44 +0900

    0623

--- shortest path/1956.cpp
+++ shortest path/1956.cpp
@@ -3,18 +3,50 @@
 #define endl '\n'
 using namespace std;
 
-#include <vector>
+#include <algorithm>
+#define INF 9999999
+int V, E, a, b, c, ans;
+int city[401][401];
 
-int V, E, a, b, c;
-vector<pair<int, int> > v;
+void floyed_warshall()
+{
+	for(int i = 1; i<=V; i++)
+	{
+		for(int j = 1; j<=V; j++)
+		{
+			for(int k = 1; k <= V; k++)
+				city[j][k] = min(city[j][k], city[j][i] + city[i][k]);
+		}
+	}
+}
+
+void init_city()
+{
+	for(int i = 1; i<=V; i++)
+	{
+		for(int j = 1; j<=V; j++)
+			city[i][j] = INF;
+	}
+}
 
 int main()
 {
 	cin >> V >> E;
+	
+	init_city();
+
 	for(int i = 0; i<E; i++)
 	{
 		cin >> a >> b >> c;
-		v[a].push_back(make_pair(b, c));
+		city[a][b] = c;
 	}
-	
+
+	floyed_warshall();
+	ans = INF;
+	for(int i = 1; i<=V; i++)
+		ans = min(ans, city[i][i]);
+	if (ans == INF)
+		cout << "-1";
+	else
+		cout << ans;				
 }
