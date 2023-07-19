f965820abdb2717e9262dd788989de4a27fa3d34
Sejin Kim <kimsejin2118@gmail.com>
2023-07-19 20:44:11 +0900

    0719

---
+++ topological sorting/2252.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define fast_io ios_base::sync_with_stdio(false), cin.tie(false), cout.tie(false)
+#define endl '\n'
+using namespace std;
+
+#include <queue>
+
+queue<int> q;
+int N;
+int incomeEdge[101];
+
+int main()
+{
+	cin >> N;
+	for(int i = 1; i<=N; i++)
+	{
+		int tier, time;
+		cin >> tier >> time;
+		
+	}
+}
