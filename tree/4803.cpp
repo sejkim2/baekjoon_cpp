9df2e0ecbe3db6b1c82f8031fb39b899d52a57e4
kimsejin <kimsejin2118@gmail.com>
2023-07-21 00:46:56 +0900

    0721

--- tree/4803.cpp
+++ tree/4803.cpp
@@ -27,29 +27,26 @@ void init_graph()
 
 int bfs(int node)
 {
-	if (visited[node] == 1)
-		return (0);
+	int flag = 1;
 	queue<int> q;
 	q.push(node);
 
 	while (!q.empty())
 	{
 		int nd = q.front();
+		if (visited[nd] == 1)
+			flag = 0;
 		visited[nd] = 1;
 		q.pop();
 
-		if (graph[nd].size() == 0) 
-			return (1);
 		for(int i = 0; i<graph[nd].size(); i++)
 		{
 			int next_node = graph[nd][i];
 			if (visited[next_node] == 0)
 				q.push(next_node);
-			else
-				return (0);
 		}
 	}
-	return (1);
+	return (flag);
 }
 
 int main()
@@ -62,15 +59,21 @@ int main()
 		if (n == 0 && m == 0)
 			break;
 		case_num++;
+		init_graph();
+		init_visited();
 		for(int i = 0; i<m; i++)
 		{
 			int vertex1, vertex2;
 			cin >> vertex1 >> vertex2;
 			graph[vertex1].push_back(vertex2);
+			graph[vertex2].push_back(vertex1);
 		}
 		count_tree = 0;
 		for(int i = 1; i<=n; i++)
-			count_tree += bfs(i);
+		{
+			if (visited[i] == 0)
+				count_tree += bfs(i);
+		}
 
 		if (count_tree == 0)
 			cout << "Case " << case_num << ": " << "No trees." << endl;
@@ -78,7 +81,5 @@ int main()
 			cout << "Case " << case_num << ": " << "There is one tree." << endl;
 		else
 			cout << "Case " << case_num << ": " << "A forest of " << count_tree << " trees." << endl;
-		init_graph();
-		init_visited();
 	}
 }
