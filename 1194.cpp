98db051a817515f3f655cc3c077fc62e48f2acf1
Sejin Kim <sejkim2@c2r9s5.42seoul.kr>
2023-07-25 20:43:56 +0900

    0725

--- 1194.cpp
+++ 1194.cpp
@@ -6,14 +6,24 @@ using namespace std;
 #include <queue>
 
 int n, m;
-int move_row[4] = {1, 1, 0, 0};
-int move_col[4] = {0. -1, 1, -1};
+int move_row[4] = {1, -1, 0, 0};
+int move_col[4] = {0, 0, 1, -1};
 char maze[51][51];
 int keys[6];
 int visited[51][51];
 
-void bfs(int row, int col)
+void init_visited()
 {
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 1; j<=m; j++)
+            visited[i][j] = 0;
+    }
+}
+
+int bfs(int row, int col)
+{
+    int walk = 0;
     queue<pair<int, int> > q;
     q.push(make_pair(row, col));
     
@@ -22,26 +32,43 @@ void bfs(int row, int col)
         int cur_row = q.front().first;
         int cur_col = q.front().second;
         q.pop();
-        visited[cur_row][cur_col] = 1;
         char ch = maze[cur_row][cur_col];
-        if (ch >= 'A' && ch <= 'F')
+
+        if (ch >= 'a' && ch <= 'f')
         {
-            if (keys[ch - 'A'] < 1)
-                return (0);
+            keys[ch - 'a'] += 1;
+            init_visited();
         }
+        else if (ch == '1')
+            return (walk);
+        visited[cur_row][cur_col] = 1;
+        walk++;
+
         for(int i = 0; i<4; i++)
         {
             int next_row = cur_row + move_row[i];
             int next_col = cur_col + move_col[i];
+            char next_ch = maze[next_row][next_col];
+
             if (next_row > n || next_row < 1 || next_col > m || next_col < 1)
                 continue;
-            
+            if (next_ch == '#' || visited[next_row][next_col] == 1)
+                continue;
+            if (next_ch >= 'A' && next_ch <= 'F')
+            {
+                if (keys[ch - 'A'] < 1)
+                    continue;
+            }
+            q.push(make_pair(next_row, next_col));
         }
     }
+    return (-1);
 }
 
 int main()
 {
+    int start_row, start_col;
+    int walk = -1;
     cin >> n >> m;
     for(int i = 1; i<=n; i++)
     {
@@ -50,10 +77,14 @@ int main()
             char ch;
             cin >> ch;
             maze[i][j] = ch;
-            if (ch >= 'A' && ch <= 'F')
-                keys[ch - 'A'] += 1;
+            if (ch == '0')
+            {
+                start_row = i;
+                start_col = j;
+            }
         }
     }
 
-
+    walk = bfs(start_row, start_col);
+    cout << walk;
 }
